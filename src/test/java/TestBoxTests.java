de48066280a61824e0d3cd8599a9ed7a21c36624
Sagittarius87 <Sagittarius87@Jarik-87@yandex.ru>
2022-05-18 20:29:01 +0300

    added dependencies "org.slf4j:slf4j-simple:1.7.36" in build.gradle

---
+++ src/test/java/TestBoxTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+/*
+public class TestBoxTests {
+    @Test
+    public void openGoogle() {
+        open("https://www.google.ru/");
+        $(byName("q")).setValue("Hello World!");
+    }
+}
+*/
