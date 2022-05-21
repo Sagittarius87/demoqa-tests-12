511fd64af5b5d4e2562aeffb983f1367734b027e
Sagittarius87 <Sagittarius87@Jarik-87@yandex.ru>
2022-05-21 17:26:18 +0300

    added a test function fillForm()

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -1,6 +1,11 @@
+import com.codeborne.selenide.ClickOptions;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+import java.io.FileWriter;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -15,8 +20,34 @@ public class TestForm {
         Configuration.holdBrowserOpen = true;
     }
 
-    @Test
+    @BeforeEach
     void openSite() {
         open("/automation-practice-form");
+    }
+
+    @Test
+    void fillForm() {
+        String firstName = "Bob";
+        String lastName = "Rob";
+        String userEmail = "Bob@sobaka.com";
+        String userNumber = "1111111111";
+        String dateOfBirthInput = "20 May 2022";
+        String subjectsInput = "M";
+        String currentAddress = "ulitsa Lesnaya, 5, kv. 176\nMoscow\nRussia\n125075";
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $(byText("Male")).click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").setValue(dateOfBirthInput).pressEnter();
+        $("#subjectsInput").setValue(subjectsInput).pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/image.jpg"));
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
     }
 }
